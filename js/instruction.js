ace92e77fe0840b2723761cae58902447e11dd4f
Andrea <andr45f9@stud.kea.dk>
2025-11-03 08:34:19 +0100

    tilføj

---
+++ js/instruction.js
@@ -0,0 +1,19 @@
+console.log("loaded instruction...");
+
+document.querySelector("#foerste").addEventListener("click", funk1);
+
+function funk1() {
+  console.log("funk1 in da house");
+}
+
+document.querySelector("#anden").addEventListener("click", funk2);
+
+function funk2() {
+  console.log("funk2 in da house");
+}
+
+document.querySelector("#tredje").addEventListener("click", funk3);
+
+function funk3() {
+  console.log("funk3 in da house");
+}
