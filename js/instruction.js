86e5031e8d147e0f0a2ce1d52ccaf1b75e525e75
Andrea <andr45f9@stud.kea.dk>
2025-11-06 12:21:25 +0100

    ekstra til nærkontakt

--- js/instruction.js
+++ js/instruction.js
@@ -6,6 +6,8 @@ function funk1() {
   console.log("funk1 in da house");
   document.querySelector("#pedal").classList.add("my_scale");
   document.querySelector(".info-text h2").textContent = "Undgå at træd i pedalerne";
+  document.querySelector(".placeholder").textContent =
+    "Det viser sig, at virussen “Cykelpest-23” ligger i dvale i alle metalpedaler, men vågner, når man stiller sig op og træder hårdt i dem.De første symptomer er ekstrem vind i håret, overdreven konkurrenceinstinkt og trang til at råbe “jeg kan komme først op af bakken!”Forskere anbefaler at skubbe sig selv frem med fødderne, eller endnu bedre lade som om man har en punktering.Observationer viser, at virussen ignorerer mennesker, der virker dovne.";
 }
 
 document.querySelector("#anden").addEventListener("click", funk2);
@@ -14,6 +16,8 @@ function funk2() {
   console.log("funk2 in da house");
   document.querySelector("#hjul").classList.add("my_scale");
   document.querySelector(".info-text h2").textContent = "Tag støttehjul på";
+  document.querySelector(".placeholder").textContent =
+    "Ifølge cykelforskere fra Aalborg Universitet kan smitten ikke trænge igennem psykologisk umodenhed.Når man monterer støttehjul, sender man et signal til virussen om, at man stadig går i børnehave og ikke kan indgå i konkurrencer.konsekvenser ved dette er lidt social ydmygelse. ";
 }
 
 document.querySelector("#tredje").addEventListener("click", funk3);
@@ -22,4 +26,6 @@ function funk3() {
   console.log("funk3 in da house");
   document.querySelector("#medalje").classList.add("my_scale");
   document.querySelector(".info-text h2").textContent = "Lad som om du har vundet ";
+  document.querySelector(".placeholder").textContent =
+    "Cykelpesten reagerer stærkt på følelsen af tab.Hvis du derimod konstant opfører dig som en vinder, mister virussen interessen – den tror, du allerede har gennemført Tour de France og forlader kroppen.Sundhedsmyndighederne anbefaler at bære en guldmedalje samt sige sætninger som: “Ja, det var en hård etape i dag, men jeg klarede den!” Undgå dog interviews – overdreven pral kan genaktivere sygdommen.";
 }
