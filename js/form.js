4624b76efe526126ae2879c7ef18302c6f9e86c3
Andrea <andr45f9@stud.kea.dk>
2025-11-07 15:06:46 +0100

    forms data

--- js/form.js
+++ js/form.js
@@ -1,42 +1,50 @@
 const form = document.querySelector("form");
 
-function cancelPopup(event) {
-  event.preventDefault();
-  form.querySelector(":user-invalid").focus();
-}
-
 form.addEventListener("invalid", cancelPopup, true);
 
-const form = document.querySelector("form");
-
-const firstNameOutput = document.querySelector("#first_name_output");
-const lastNameOutput = document.querySelector("#last_name_output");
-const emailOutput = document.querySelector("#email_output");
-const passwordOutput = document.querySelector("#password_output");
-const termsOutput = document.querySelector("#terms_output");
-
 function cancelPopup(event) {
   event.preventDefault();
   form.querySelector(":user-invalid").focus();
 }
+
+const firstNameOutput = document.querySelector("#first_name_output");
+const numberOutput = document.querySelector("#number_output");
+const postnummerOutput = document.querySelector("#postnummer_output");
+const termsOutput = document.querySelector("#terms_output");
+const emailOutput = document.querySelector("#email_output");
+const telefonOutput = document.querySelector("#telefon_output");
+const koenOutput = document.querySelector("#koen_output");
+const cykelOutput = document.querySelector("#cykel_output");
+const henvisningOutput = document.querySelector("#henvisning_output");
 
 function handleSubmit(event) {
   event.preventDefault();
 
+  // 1. Saml værdierne fra formularen
   const formData = new FormData(form);
   const firstName = formData.get("first-name");
-  const lastName = formData.get("last-name");
+  const number = formData.get("number");
+  const postnummer = formData.get("zipcode");
+  const terms = formData.get("accept_terms");
   const email = formData.get("email");
-  const password = formData.get("password");
-  const accept = formData.get("accept_terms");
+  const telefon = formData.get("telefon");
+  const koen = formData.get("gender");
+  const cykel = formData.get("message");
+  const henvisning = formData.get("myndighed");
+
+  // 2. Vis værdierne i de rigtige output-felter
 
   firstNameOutput.textContent = firstName;
-  lastNameOutput.textContent = lastName;
+  numberOutput.textContent = number;
+  postnummerOutput.textContent = postnummer;
+  termsOutput.textContent = terms;
   emailOutput.textContent = email;
-  passwordOutput.textContent = password;
-  termsOutput.textContent = accept;
+  telefonOutput.textContent = telefon;
+  koenOutput.textContent = koen;
+  cykelOutput.textContent = cykel;
+  henvisningOutput.textContent = henvisning;
+
   form.reset();
 }
 
-form.addEventListener("invalid", cancelPopup, true);
 form.addEventListener("submit", handleSubmit);
